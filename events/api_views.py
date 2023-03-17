fccbe27a15999c855e665586341ac151ff08fd40
hanna <hmae221@gmail.com>
2023-03-17 09:23:26 -0500

    Minor changes, removed some docstrings

--- events/api_views.py
+++ events/api_views.py
@@ -4,24 +4,6 @@ from .models import Conference, Location
 
 
 def api_list_conferences(request):
-    """
-    Lists the conference names and the link to the conference.
-
-    Returns a dictionary with a single key "conferences" which
-    is a list of conference names and URLS. Each entry in the list
-    is a dictionary that contains the name of the conference and
-    the link to the conference's information.
-
-    {
-        "conferences": [
-            {
-                "name": conference's name,
-                "href": URL to the conference,
-            },
-            ...
-        ]
-    }
-    """
     response = []
     conferences = Conference.objects.all()
     for conference in conferences:
@@ -35,30 +17,6 @@ def api_list_conferences(request):
 
 
 def api_show_conference(request, id):
-    """
-    Returns the details for the Conference model specified
-    by the id parameter.
-
-    This should return a dictionary with the name, starts,
-    ends, description, created, updated, max_presentations,
-    max_attendees, and a dictionary for the location containing
-    its name and href.
-
-    {
-        "name": the conference's name,
-        "starts": the date/time when the conference starts,
-        "ends": the date/time when the conference ends,
-        "description": the description of the conference,
-        "created": the date/time when the record was created,
-        "updated": the date/time when the record was updated,
-        "max_presentations": the maximum number of presentations,
-        "max_attendees": the maximum number of attendees,
-        "location": {
-            "name": the name of the location,
-            "href": the URL for the location,
-        }
-    }
-    """
     conference = Conference.objects.get(id=id)
     return JsonResponse(
         {
