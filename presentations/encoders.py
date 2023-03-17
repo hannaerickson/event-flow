2e634d69d10b82ec6195291eb06b5adf659b18e1
hanna <hmae221@gmail.com>
2023-03-17 13:55:18 -0500

    Upated with encoders

--- presentations/encoders.py
+++ presentations/encoders.py
@@ -1,5 +1,6 @@
 from common.json import ModelEncoder
 from .models import Presentation
+from events.encoders import ConferenceListEncoder
 
 
 class PresentationListEncoder(ModelEncoder):
@@ -22,8 +23,11 @@ class PresentationDetailEncoder(ModelEncoder):
         "title",
         "synopsis",
         "created",
-        "status",
+        "conference",
     ]
+    encoders = {
+        "conference": ConferenceListEncoder(),
+    }
 
     def get_extra_data(self, o):
         return {"status": o.status.name}
